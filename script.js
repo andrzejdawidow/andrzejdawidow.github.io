dfd0ac60390502ced5965ffa39ae6de5b555137e
andrzejdawidow <andrzejdawidow@gmail.com>
2018-04-03 17:42:36 +0200

    zad.19.1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://guarded-lake-36052.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'https://afternoon-dusk-28697.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://afternoon-dusk-28697.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
